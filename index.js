b1e28cc2855ae96cc4e97c31a3b5494cdfec837e
Pranav Chaturvedi <chaturvedipranava@gmail.com>
2025-07-01 18:48:04 +0530

    ok

--- index.js
+++ index.js
@@ -8,3 +8,4 @@ console.log("This is the third line of the index.js file.");
 console.log("This is the fourth line of the index.js file.");
 console.log("This is the fifth line of the index.js file.");
 console.log("This is the sixth line of the index.js file.");
+console.log("This is the seventh line of the index.js file.");
